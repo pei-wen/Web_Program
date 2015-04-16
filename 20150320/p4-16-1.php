716cf51a11af8ad944ebd46e49811092d8041f67
unknown <you@example.com>
2015-04-16 21:32:13 +0800

    善用陣列完成下拉是選單以及單選

---
+++ 20150320/p4-16-1.php
@@ -0,0 +1,28 @@
+﻿<html>
+<head>
+<meta charset="utf-8">
+<title>
+</title>
+</head>
+<body>
+<?php
+$d=array("台東","花蓮","宜蘭","台北","桃園","新竹","台中","南投","彰化","雲林","台南","高雄","屏東");
+?>
+<form method="post" action=p4-14.php>
+<select name="city">
+    <option>--請選擇--</option>
+	<?php
+	for($i=0;$i<count($d);$i++){
+	echo "<option>" . $i . "-" . $d[$i] . "</option>";
+    }
+	?>
+</select>
+<?php
+echo "<br>";
+echo count($d);
+echo "<br>";
+?>
+<input type="submit" value="送出">
+</form>
+</body>
+</html>
---
+++ 20150320/p4-16.php
@@ -0,0 +1,24 @@
+<html>
+<meta charset="utf8">
+<head>
+</head>
+<body>
+<?php
+$a=12345;
+echo $a;
+echo "<br>";
+$b="php";
+echo $b;
+echo "<br>";
+$d=array("甲","乙","丙");
+echo $d[0];
+echo "<br>";
+echo $d[1];
+echo "<br>";
+echo $d[2];
+echo "<br>";
+?>
+陣列索引
+陣列值
+</body>
+</html>
